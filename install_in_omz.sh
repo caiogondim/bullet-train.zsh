9dec5868844f9ccf1cd08481626bfc5283479c83
Jérémy Romey <jeremy@free-agent.fr>
2012-11-15 10:53:33 +0100

    Updated install script

--- install_in_omz.sh
+++ install_in_omz.sh
@@ -33,4 +33,5 @@ POWERLINE_DETECT_SSH=\"true\"
 
 Requirements:
 Vim Powerline patched font: See Powerline for vim for more info.
-Z shell (zsh): See oh-my-zsh for more info."
+Z shell (zsh): See oh-my-zsh for more info.
+Make sure terminal is using 256-colors mode with export TERM=\"xterm-256color\"".
